d2afd7e64e033b416340724a1f0f845bf75c2f6c
tobiolusa <olusamiracle@gmail.com>
2023-09-17 04:27:10 +0100

    restframework

--- blognook/settings.py
+++ blognook/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     ## Newly added apps 
-    'accounts'
+    'rest-framework',
+    'accounts',
 ]
 
 MIDDLEWARE = [
