6342cc82f61d5009a0c2f3320302d29fdc6ae165
tobiolusa <olusamiracle@gmail.com>
2023-09-17 18:03:41 +0100

    Swagger ui implementation

--- blognook/settings.py
+++ blognook/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'accounts',
     ## Newly added libraries 
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- blognook/urls.py
+++ blognook/urls.py
@@ -15,7 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blognook v1 API",
+        default_version= "v1",
+        description= "Blognook is a cms blog api that allow users to create account and have their personal blog on the app which enable users to post, edit and delete their content",
+        contact= openapi.Contact(email="olusamiracle@gmail.com")
+    ), 
+    public = True,
+    permission_classes = (permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name="schema-redoc")
 ]
