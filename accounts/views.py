93d921d1148097329876e0a7b448eb151e71772b
tobiolusa <olusamiracle@gmail.com>
2023-09-18 20:40:37 +0100

    login auth done with token authentication

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,10 @@ from .serializers import UserSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from django.core.expections import ObjectDoesNotExist
+from .models import CustomUser
 
 @api_view(['POST'])
 def register_user(request):
@@ -11,4 +15,23 @@ def register_user(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors), status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['POST'])
+def user_login(request):
+    if request.method == 'POST':
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        user = None
+        if '@' in username:
+            try: 
+                user = CustomUser.objects.get(email=username)
+            except ObjectDoesNotExist
+                pass 
+        
+        if not user:
+            user = authenticate(username=username, password=password)
+        id user:
+            token = Token.objects.get_or_create(user=user)
+            return Response({'errors': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
