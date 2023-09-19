f42e0f62d221441826687d72ca61c9e22444d301
tobiolusa <olusamiracle@gmail.com>
2023-09-19 23:01:31 +0100

    User Profile added

--- accounts/models.py
+++ accounts/models.py
@@ -7,3 +7,15 @@ class CustomUser(AbstractUser):
     
     def __str__(self):
         return self.username
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser,related_name='userprofile', on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    birth_date = models.DateTimeField(null=True, blank=True)
+    blog_logo = models.ImageField(upload_to='profile_picture', null=True, blank=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+    
