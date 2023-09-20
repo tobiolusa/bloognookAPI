b333397c0db91214b8198712307665a6a1f01ab0
tobiolusa <olusamiracle@gmail.com>
2023-09-20 16:10:54 +0100

    Blog models and profile picture path added

--- blogcms/admin.py
+++ blogcms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import BlogPost, PostComment, PostCategory
+from .models import BlogPost, PostComment
 
 admin.site.register(BlogPost)
+admin.site.register(PostComment)
--- blogcms/models.py
+++ blogcms/models.py
@@ -4,6 +4,7 @@ from accounts.models import CustomUser
 class BlogPost(models.Model):
     title = models.CharField(max_length=256, blank=False, null=False)
     content = models.TextField(max_length=256, blank=False, null=False)
+    
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=False)
     author  = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
@@ -11,8 +12,7 @@ class BlogPost(models.Model):
     def __str__(self):
         return self.title
     
-class PostCategory(models.Model):
-    pass
+
 
 class PostComment(models.Model):
     blog = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
