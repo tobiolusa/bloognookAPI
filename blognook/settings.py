8da27b27a4d17306ec9a8da1a6367b23d291ce93
tobiolusa <olusamiracle@gmail.com>
2023-09-22 00:58:13 +0100

    settings updated

--- blognook/settings.py
+++ blognook/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL= 'accounts.CustomUser'
+AUTH_USER_MODEL= 'accounts.CustomUser' 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication', 
