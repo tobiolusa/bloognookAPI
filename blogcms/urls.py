8ebc0ab2cdd4f080a889e0b134e0f9eaeab02800
tobiolusa <olusamiracle@gmail.com>
2023-09-22 03:56:01 +0100

    perimmission updated

--- blogcms/urls.py
+++ blogcms/urls.py
@@ -6,6 +6,6 @@ from .views import PostDetail, PostList
 urlpatterns = [
   path('posts/', PostList.as_view(), name="posts"),
   path('posts/<int:pk>', PostList.as_view(), name="post-details"),
-  path('posts/<int:pk>', PostDetail.as_view(), name="post-details")
+  path('posts/<int:pk>',PostDetail.as_view(), name="post-details")
   
 ]
--- blogcms/views.py
+++ blogcms/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import BlogPost
 from .serializers import BlogPostSerializer
+from rest_framework import permissions
+from .permissions import IsOwnerOrReadOnly
 
 class PostList(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
     
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -13,3 +16,5 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+    permissions_classes = ['IsOwnerOrReadOnly', 'IsAuthenticatedOrReadOnly']
+
